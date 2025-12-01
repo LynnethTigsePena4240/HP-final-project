c58ad7ac67e69149b8b0d4ecce353b3097407f48
LynnethTigsePena4240 <lynneth_tigsepena@hotmail.com>
2025-12-01 13:32:49 -0500

    adding other basic files to start the project

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book (models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    year = models.IntegerField()
+    rating = models.IntegerField()
+    description = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.title
