64eda726afd8462a60c8ee448be9590048c7cc60
LynnethTigsePena4240 <lynneth_tigsepena@hotmail.com>
2025-12-01 15:17:36 -0500

    added register and login function and other empty html files to use in the project later on

---
+++ books/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('book_detail/<int:id>/', views.book_detail, name = 'book_detail'),
+
+    path('add/', views.add_book, name = 'add_book'),
+    path('edit/<int:id>/', views.edit_book, name = 'edit_book'),
+    path('delete/<int:id>', views.delete_book, name = 'delete_book'),
+]
--- books/views.py
+++ books/views.py
@@ -2,3 +2,25 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def homepage(request):
+
+    return render(request,'books/homepage.html')
+
+def book_detail(request):
+
+    return render(request,'books/book_detail.html')
+
+# @login_required (uncomment later when add book is working)
+def add_book(request):
+
+    return render(request,'books/add_edit_book.html')
+
+# @login_required (uncomment later when edit book is working)
+def edit_book(request):
+
+    return render(request,'books/add_edit_book.html')
+
+# @login_required (uncomment later when delete book is working)
+def delete_book(request):
+
+    return render(request,'books/add_edit_book.html')
