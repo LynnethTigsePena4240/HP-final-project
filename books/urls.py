76b5b0fac9d33004cc5a31d7c5f4832184eac38b
lena mukhtar <lena.mukhtar@gmail.com>
2025-12-06 22:47:01 -0500

    added the add/edit/delete

--- books/urls.py
+++ books/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('book_detail/<int:id>/', views.book_detail, name = 'book_detail'),
+    path('book_detail/<int:book_id>/', views.book_detail, name = 'book_detail'),
 
     path('add/', views.add_book, name = 'add_book'),
-    path('edit/<int:id>/', views.edit_book, name = 'edit_book'),
-    path('delete/<int:id>', views.delete_book, name = 'delete_book'),
+    path('edit/<int:book_id>/', views.edit_book, name = 'edit_book'),
+    path('delete/<int:book_id>', views.delete_book, name = 'delete_book'),
     path('api/books/', views.BookList.as_view(),name='book-list-create'),
     path('api/books/<int:pk>/', views.BookDetailView.as_view(),name='book-detail')
 ]
--- books/views.py
+++ books/views.py
@@ -2,31 +2,49 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from rest_framework import generics
 from .models import Book
+from .forms import BookForm
 from .serializers import BookSerializer
 
 # Create your views here.
 def homepage(request):
+    books = Book.objects.all()
+    return render(request,'books/homepage.html', {'books':books})
 
-    return render(request,'books/homepage.html')
-
-def book_detail(request):
-
-    return render(request,'books/book_detail.html')
+def book_detail(request,book_id):
+    books = Book.objects.get(id=book_id)
+    return render(request,'books/book_detail.html', { 'book':books})
 
 # @login_required (uncomment later when add book is working)
 def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
+    else:
+        form = BookForm()
 
-    return render(request,'books/add_edit_book.html')
+    return render(request, 'books/add_edit_book.html', {'form': form, 'new_book': True})
+
 
 # @login_required (uncomment later when edit book is working)
-def edit_book(request):
-
-    return render(request,'books/add_edit_book.html')
+def edit_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    form = BookForm(request.POST or None, instance = book)
+    if form.is_valid():
+        form.save()
+        return redirect('homepage')
+    
+    return render(request,'books/add_edit_book.html', {'form':form, 'new_book':False})
 
 # @login_required (uncomment later when delete book is working)
-def delete_book(request):
+def delete_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.method == 'POST': 
+        book.delete()
+        return redirect('homepage')
 
-    return render(request,'books/add_edit_book.html')
+    return render(request,'books/add_edit_book.html', {'book':book})
 
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
